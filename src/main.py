ec54cdc9ba0b66c822873820c7b06d5d097e6810
Griffin <pfenniger.griffin@gmail.com>
2018-07-03 20:55:27 -0700

    base

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class CarWars(Widget):
+    pass
+
+
+class CarWarsApp(App):
+    def build(self):
+        return CarWars()
+
+
+if __name__ == '__main__':
+    CarWarsApp().run()
